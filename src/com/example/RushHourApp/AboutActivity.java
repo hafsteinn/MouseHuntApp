bc8eeab4eb39114d95a900e45c4f2e532a441e3d
hafsteinn <hafsteinn11@ru.is>
2013-10-21 17:17:16 +0000

    initial commit

---
+++ src/com/example/RushHourApp/AboutActivity.java
@@ -0,0 +1,24 @@
+package com.example.RushHourApp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hafsteinn11
+ * Date: 20.10.2013
+ * Time: 23:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class AboutActivity extends Activity{
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+
+}
---
+++ src/com/example/RushHourApp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.RushHourApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void startAboutActivity(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+
+     /*
+    public void buttonPressed( View view )
+    {
+        Button buttonView = (Button) view;
+        switch ( view.getId() ) {
+            case R.id.buttonPLAY:
+                    //what to do if play button is pressed ?
+                break;
+            case R.id.buttonPuzzles:
+                    //what to do if puzzles button is pressed ?
+                break;
+            case R.id.buttonOptions:
+                    //what to do if Options button is pressed ?
+                break;
+            case R.id.buttonAbout:
+                    startAboutActivity(view);
+               // showabout();
+                break;
+
+        }
+
+    }
+    */
+}
