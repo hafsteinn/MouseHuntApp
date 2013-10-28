65f154d93231d84e701d1ca5f06e381b08557073
hafsteinn <hafsteinn11@ru.is>
2013-10-28 09:26:26 +0000

    added drawView to play button

---
+++ src/com/example/RushHourApp/BoardActivity.java
@@ -0,0 +1,18 @@
+package com.example.RushHourApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hafsteinn11
+ * Date: 28.10.2013
+ * Time: 08:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class BoardActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.boardview);
+    }
+}
--- src/com/example/RushHourApp/MainActivity.java
+++ src/com/example/RushHourApp/MainActivity.java
@@ -25,6 +25,11 @@ public class MainActivity extends Activity {
         startActivity(intent);
     }
 
+    public void startBoardActivity(View view) {
+        Intent intent = new Intent(this, BoardActivity.class);
+        startActivity(intent);
+    }
+
 
      /*
     public void buttonPressed( View view )
